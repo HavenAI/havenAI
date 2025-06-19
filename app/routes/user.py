288ccf2ec926b00146c95db9a19c30db7b519a67
TharunyaSundar <tsundar@binghamton.edu>
2025-06-18 22:00:36 -0400

    Fetch and return Firebase user metadata by UID

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,16 +1,31 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, FastAPI
 from datetime import datetime
 from app.firebase_auth import verify_token
 from app.db import get_db
 from app.models.user_model import OnboardingAnswers  # import your new model
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from app.services.user_quiz import get_quiz_data
+from firebase_admin import auth, initialize_app
+
 
 router = APIRouter()
 auth_scheme = HTTPBearer()
 
+
 @router.get("/user/answers")
 def get_answers(credentials: HTTPAuthorizationCredentials = Depends(auth_scheme)):
     data = get_quiz_data(credentials)
-    print(data)
     return data
+
+
+@router.get("/user/{uid}")
+def get_user_details(uid: str):
+    user = auth.get_user(uid)
+    print(uid)
+    print(user)
+    return {
+        "uid": user.uid,
+        "email": user.email,
+        "created_at": user.user_metadata.creation_timestamp,
+        "last_sign_in": user.user_metadata.last_sign_in_timestamp
+}
