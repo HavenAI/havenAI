f21d6e2eada7b511e9578c8a99f8232f9966704c
TharunyaSundar <tsundar@binghamton.edu>
2025-06-29 02:18:17 -0400

    added money saved route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer
-from app.routes import logs, chat, onboarding, user, daily_checkin, rating, checkin_status
+from app.routes import logs, chat, onboarding, user, daily_checkin, rating, checkin_status, money_saved
 
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBearer
